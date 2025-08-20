cd88fe0e860b72d9aedf1ee1122ee146c35b0458
Arjan Regmi <arjanregmi2006@gmail.com>
2025-08-20 18:30:14 +0545

    Added the toggle bar as well as the sign up section

--- JS/script.js
+++ JS/script.js
@@ -55,3 +55,22 @@
     
 
 })(jQuery);
+
+const loginBtn = document.getElementById('loginBtn');
+const signupBtn = document.getElementById('signupBtn');
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+
+loginBtn.addEventListener('click', () => {
+    loginForm.style.display = 'block';
+    signupForm.style.display = 'none';
+    loginBtn.classList.add('active');
+    signupBtn.classList.remove('active');
+});
+
+signupBtn.addEventListener('click', () => {
+    loginForm.style.display = 'none';
+    signupForm.style.display = 'block';
+    signupBtn.classList.add('active');
+    loginBtn.classList.remove('active');
+});
